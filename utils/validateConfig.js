75d404e84f81efcdb031d1950a26f41ba71d7d5b
Manh Le <lemanh2006@gmail.com>
2026-02-03 20:28:20 +0700

    add log

--- utils/validateConfig.js
+++ utils/validateConfig.js
@@ -115,6 +115,8 @@ function validateCredentials(config) {
  */
 function validateConfig(config) {
   console.log('\n=== Validating Configuration ===');
+  if(process.env.NODE_ENV === 'stage') {
+  console.log('config', config);
 
   validateRealm(config.realm);
   validateAuthServers(config.auth_servers);
